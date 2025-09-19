96d1b311677f409cc164286def2b2ae0bc890151
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-19 04:40:23 +0100

    init commit

---
+++ src/test/java/dev/engripaye/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
